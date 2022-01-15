29ae3508e8ae16870d5f28cc336d92c32782935b
Keon White <keon-1@hotmail.co.uk>
2022-01-15 14:48:02 +0000

    Create a simple task app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # C
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # R
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # U
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # D
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
